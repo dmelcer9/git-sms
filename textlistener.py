e228f13ac750f2f76608da4061021c95a16ce643
root <root@debian-s-1vcpu-1gb-nyc1-01.localdomain>
2018-11-11 10:39:42 +0000

    Handle receiving images

--- textlistener.py
+++ textlistener.py
@@ -17,16 +17,22 @@ app = Flask(__name__)
 def incoming_sms():
     
     body = request.values.get('Body', None)
-    bodylines = [">>" + x for x in  body.split("\n")]
+    bodylines = [">>" + x for x in  body.split("\n")] if len(body) > 0 else []
 
     phonenum = request.values.get("From", None)
     phonenum = phonenum.replace("+", "")
+
+    print(str(request.values))
 
     remote.pull()
 
     print(phonenum)
     print(body)
-    
+
+    numMedia = int(request.values.get('NumMedia', None))
+    for media in range(numMedia):
+        bodylines.append(">>Attachment: " + request.values.get('MediaUrl' + str(media), None))
+
     with open("/tmp/smsrepo/" + phonenum, "a") as file:
         file.write("\n" + "\n".join(bodylines) + "\n")
 
