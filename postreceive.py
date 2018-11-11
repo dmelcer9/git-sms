5a76d31aa4ef33b250537962a663a25ba94dd80a
root <root@debian-s-1vcpu-1gb-nyc1-01.localdomain>
2018-11-11 07:08:41 +0000

    fixed diff parsing

--- postreceive.py
+++ postreceive.py
@@ -8,10 +8,16 @@ from unidiff import PatchSet
 
 def process_commits_between(old, new):
     repo = Repo(os.environ["GIT_REPO_PATH"])
-    end = repo.commit(new)
-    for diff in end.diff(old, create_patch=True).iter_change_type('A'):
-        for message in diff.b_blob.data_stream.read().decode("utf-8").strip().split("\n"):
-                print("Send message %s to %s" % (message, diff.b_path))
+    rawdiff = repo.git.diff(old, new)
+
+    parseddiff = PatchSet(rawdiff)
+
+    for file in parseddiff:
+        for hunk in file:
+            for line in hunk.target_lines():
+                if line.is_added and line.value.strip() != "" :
+                    print("Send message %s to %s" % (line.value.strip(), os.path.split(file.target_file)[1]))
+                
                 
 
 def main():
@@ -21,5 +27,5 @@ def main():
             process_commits_between(old, new)
 
 if __name__ == '__main__':
-    process_commits_between("39439cfac68cd2acd6ae8363214d3339ee241006", "10eedc1865fb403018ab9bb91761f78a9d5da8fa") 
+    process_commits_between("10eedc1865fb403018ab9bb91761f78a9d5da8fa", "9f62fed1fc885ecac65adec9f1e9aee824665db9")
     # main()
